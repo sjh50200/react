c4c6bc241a4268d150acea5f273bf1c0dc9d56a3
jjong <sjh2273@naver.com>
2023-01-25 22:40:42 +0900

    feat: google sign in button 구현

--- google-sign-in/src/App.js
+++ google-sign-in/src/App.js
@@ -1,8 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+export const GoogleButtonContainer = styled.div`
+  width: 200px;
+  height: 50px;
+  border: 1px solid black;
+  #g_id_siginin {
+    width: 100px;
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GoogleButtonContainer>
+        <div
+          id="g_id_onload"
+          data-client_id={process.env.REACT_APP_GSI_ID}
+          data-callback="handleCredentialResponse"
+        ></div>
+        <div class="g_id_signin" data-type="standard"></div>
+        hi
+      </GoogleButtonContainer>
+    </div>
+  );
 }
 
 export default App;

d310757af0e90cf1f3dfa44e20359bfd09cbb023
sjh50200 <sjh2273@naver.com>
2023-04-16 17:37:37 +0900

    design: 페이지에 노출될 컴포넌트들 중앙 정렬(main 태그 추가

--- design-patterns/src/component/compound/index.tsx
+++ design-patterns/src/component/compound/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ImageDatas } from '../../assets/mockData';
 import FlyOutMenu from './component/FlyOutMenu';
 import ImageBox from './component/ImageBox';
 
+export const StyledMain = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3rem;
+`;
+
 const Compound = () => {
   return (
-    <ImageBox imageData={ImageDatas[0]}>
-      <FlyOutMenu />
-    </ImageBox>
+    <StyledMain>
+      <ImageBox imageData={ImageDatas[0]}>
+        <FlyOutMenu />
+      </ImageBox>
+    </StyledMain>
   );
 };
 

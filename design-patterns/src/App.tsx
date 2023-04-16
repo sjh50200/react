70258a96bf74e6a77b95bc1ad6340dd054900e7c
sjh50200 <sjh2273@naver.com>
2023-04-16 17:29:41 +0900

    feat: global style app.tsx에 추가

--- design-patterns/src/App.tsx
+++ design-patterns/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 // import { RouterProvider } from 'react-router-dom';
 import Router from './component/Router';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
